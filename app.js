779d819a5ee822589f6c056f6a4172402cbb844b
Donald Serafim <donald.serafim@gmail.com>
2017-10-04 16:53:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = require('./config/express')();
 
 app.listen(3000, function() {
-    console.log("servidor rodando");    	
-	console.log(process.env);
+    console.log("servidor rodando");
 })
